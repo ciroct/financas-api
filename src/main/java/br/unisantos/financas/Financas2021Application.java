b2955f5c1523d4c4b20db2758c29bf0c70c4b741
Ciro Cirne Trindade <ciroct@unisantos.br>
2021-04-05 21:55:33 -0300

    API REST para a classe Conta

---
+++ src/main/java/br/unisantos/financas/Financas2021Application.java
@@ -0,0 +1,13 @@
+package br.unisantos.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Financas2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Financas2021Application.class, args);
+	}
+
+}
